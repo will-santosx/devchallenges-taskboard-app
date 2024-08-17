e8f8d71a9db69d5fc71056cb5728466ef350bf6a
will-santosx <taldowill29@gmail.com>
2024-08-17 13:19:19 -0400

    feat: simplify task creation logic by removing request validation

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,53 +1,34 @@
 import { NextResponse } from "next/server";
 import prisma from "@/database";
 
-type TaskPayload = {
-    title: string,
-    description: string,
-    icon: 1 | 2 | 3 | 4,
-    status: "todo" | "wontdo" | "progress" | "done"
-}
-
 export async function GET() {
-    try {
-        const tasks = await prisma.task.findMany();
-        return NextResponse.json({ tasks });
-    } catch (error) {
-        return NextResponse.json({ error: "Failed to retrieve tasks" }, { status: 500 });
-    }
+  try {
+    const tasks = await prisma.task.findMany();
+    return NextResponse.json({ tasks });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to retrieve tasks" },
+      { status: 500 }
+    );
+  }
 }
 
 export async function POST(req: Request) {
-    try {
-        const body: TaskPayload = await req.json();
+  try {
+    const task = await prisma.task.create({
+      data: {
+        title: "Nova Ativade",
+        description: "",
+        icon: 1,
+        status: "todo",
+      },
+    });
 
-        if (!body.title || !body.description || !body.icon || !body.status) {
-            return NextResponse.json(
-                { error: "Invalid request. Check the parameters." },
-                { status: 400 }
-            );
-        }
-
-        const sameNameTask = await prisma.task.findFirst({ where: { title: body.title } });
-
-        if (sameNameTask) {
-            return NextResponse.json(
-                { error: "A task with the same name already exists." },
-                { status: 400 }
-            );
-        }
-
-        const task = await prisma.task.create({
-            data: {
-                title: body.title,
-                description: body.description,
-                icon: body.icon,
-                status: body.status,
-            },
-        });
-
-        return NextResponse.json({ task }, { status: 201 });
-    } catch (error) {
-        return NextResponse.json({ error: "Failed to create task" }, { status: 500 });
-    }
+    return NextResponse.json({ task }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to create task" },
+      { status: 500 }
+    );
+  }
 }
